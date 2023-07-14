f53994b43580d52786471d66868551e99b5e1144
Etienne Canales <ECANALES@onp.gob.pe>
2023-07-14 12:42:04 -0500

    NuevaBD

--- src/Recursos/Conexion.java
+++ src/Recursos/Conexion.java
@@ -11,9 +11,9 @@ public class Conexion {
     public static PreparedStatement pps;
     public static Connection getConexion(){
         
-        String url = "jdbc:postgresql://localhost:5432/ProyectosOTI";
-        String usuario = "postgres";
-        String contraseña = "PINKYPANKY";
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=ProyectosOTI";
+        String usuario = "sa";
+        String contraseña = "PinkyPanky2023";
         try{
             Connection connexion = DriverManager.getConnection(url,usuario,contraseña);
             return connexion;
--- src/Recursos/Pruebita.java
+++ src/Recursos/Pruebita.java
@@ -4,7 +4,7 @@ package Recursos;
 public class Pruebita {
     public static void main(String[] args) {
     controlador control = new controlador();
-    String Nombre = control.DevolverRegistroBD("Select actividad from act_realizada",1);
+    String Nombre = control.DevolverRegistroBD("Select cod_proy from cartera_proyectos",1);
     System.out.println(Nombre);
 
     } 
